ff903cb549e7327a81d082cba94fdde7d72a2a72
Saji <sewangco@gmail.com>
2020-10-21 10:18:21 +0200

    sumOfDiff

---
+++ 8kyu/SumOfDifferences.js
@@ -0,0 +1,11 @@
+function sumOfDifferences(arr) {
+  let sum = 0;
+  arr.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length - 1; i++) {
+    sum += arr[i] - arr[i + 1];
+  }
+  return sum;
+}
+
+//sumOfDifferences([1, 2, 10])
+//expected : 9
