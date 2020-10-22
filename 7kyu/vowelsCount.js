a3f883492a27c0cdf66bdea4d351ed23d672b1a4
Saji <sewangco@gmail.com>
2020-10-22 14:58:05 +0200

    vowelsCount

---
+++ 7kyu/vowelsCount.js
@@ -0,0 +1,18 @@
+function getCount(str) {
+  var vowelsCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      vowelsCount++;
+    }
+  }
+  return vowelsCount;
+}
+
+// getCount("abracadabra")
+//expected : 5
