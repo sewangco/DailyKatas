593dccfb904fcd59e2c40fb128515b0c1dc57028
Saji <sewangco@gmail.com>
2020-10-23 16:38:47 +0200

    pandemia

---
+++ 7kyu/pandemia.js
@@ -0,0 +1,20 @@
+//Pandemia 
+
+function infected(s) {
+    let continents = s.split('X')
+    let total = 0;
+    let infected = 0;
+    for (let continent of continents) {
+        return continent
+        total += continent.length;
+        if (continent.includes('1')) {
+            infected += continent.length
+        }
+    }
+    if (total === 0) { return 0 };
+    return (infected / total) * 100
+
+}
+
+infected("01000000X000X011X0X")
+    //expected: 73.33333333333333,
