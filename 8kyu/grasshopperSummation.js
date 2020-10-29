85c0ed6355f38690dad8a1767fa9658409840164
Saji <sewangco@gmail.com>
2020-10-29 18:33:43 +0100

    grasshopperSum

---
+++ 8kyu/grasshopperSummation.js
@@ -0,0 +1,14 @@
+//grasshopper - summation 
+
+var summation = function (num) {
+    let sum=0
+      for(let i=1;i<=num; i++){
+      sum += i
+      
+      }
+      return sum
+    }
+
+//summation(8)
+// exp: 36
+
