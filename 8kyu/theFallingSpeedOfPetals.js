dce91b2b030934a31e3f6699bd2aa42af020ba82
Saji <sewangco@gmail.com>
2020-11-06 22:59:17 +0100

    TheFallingSpeedOfPetals

---
+++ 8kyu/theFallingSpeedOfPetals.js
@@ -0,0 +1,17 @@
+function sakuraFall(v) {
+    if(v > 0){
+    return 400/v
+    }
+    if(v <= 0){
+    return 0
+    }
+    }
+
+
+    // alt 
+    function sakuraFall(v) {
+        return v <1 ? 0 : 400/v
+      }
+
+//sakuraFall(5)
+//exp: 80
