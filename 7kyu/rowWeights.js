0a0f27fc29db579808bd3f63754be7d8eb0d82ed
Saji <sewangco@gmail.com>
2020-11-23 21:14:59 +0100

    rowWeights

---
+++ 7kyu/rowWeights.js
@@ -0,0 +1,25 @@
+function rowWeights(array){
+    let team =[0,0]
+    for( let ar in array){
+      ar%2 === 0? 
+        team[0] += array[ar] : team[1] += array[ar]
+    }
+    return team
+  }
+  // alt 
+
+  function rowWeights(array){
+    let team1= 0;
+    let team2=0;
+    for( let ar in array){
+      if(ar%2 === 0){
+        team1 += array[ar]
+      } else {
+        team2+= array[ar]
+      }
+    }
+    return [team1,team2]
+  }
+
+//rowWeights([50,60,70,80])
+// [120,140]
