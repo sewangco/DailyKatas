73cb053aa891260d09f3255d7104db3d15fd6e4a
Saji <sewangco@gmail.com>
2020-12-04 19:34:13 +0100

    willThereBeSpace

---
+++ 8kyu/willThereBeSpace.js
@@ -0,0 +1,13 @@
+function enough(cap, on, wait) {
+    if(cap-on-wait > 0){
+      return 0
+    }else { return wait+ on - cap}
+  }
+
+  //alt
+
+  function enough(cap, on, wait) {
+    return wait+on >cap? wait+on -cap : 0
+   }
+   //enough(100, 60, 50)
+   //exp :10)
