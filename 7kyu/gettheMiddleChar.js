7edeefdd792ae02ff6ebb2333da8be1f72dcf297
Saji <sewangco@gmail.com>
2020-12-06 17:02:17 +0100

    alternativeSolution

--- 7kyu/gettheMiddleChar.js
+++ 7kyu/gettheMiddleChar.js
@@ -4,3 +4,12 @@ function getMiddle(s){
 
  //getMiddle("testings")
 // exp: "t"
+
+function getMiddle(s)
+{
+  if(s.length%2 === 0){
+    return s.charAt((s.length-1)/2)+s.charAt(s.length/2)
+  } else {
+    return s.charAt((s.length-1)/2)
+  }
+}
