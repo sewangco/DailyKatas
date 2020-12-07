32c4e17e8643c419f060a0153ba64451fd250dc4
Saji <sewangco@gmail.com>
2020-12-07 15:44:43 +0100

    quarterOfTheYear

---
+++ 8kyu/quarterOfYear.js
@@ -0,0 +1,24 @@
+
+// const quarterOf = (month) => {
+  
+// if(month <= 3){
+//   return 1
+// } else if (month <= 6 ){
+// return 2 
+// } 
+//   else if (  month <= 9){
+//   return 3
+// } else if(month <= 12){
+//   return 4 
+// }
+  
+// }
+
+const quarterOf = (month) => {
+  
+    return Math.ceil(month/3)
+    }
+    
+
+//quarterOf(2)
+// exp : 3
