d5b53de788c478139c16bbca8153d22a208842cc
Saji <sewangco@gmail.com>
2020-12-21 20:28:34 +0100

    dollarsAndCent

---
+++ 8kyu/dollarsAndCent.js
@@ -0,0 +1,19 @@
+function formatMoney(amount){
+    return '$' + amount.toFixed(2);
+  }
+
+
+  //formatMoney(39.99)
+  // exp : '$39.99'
+
+
+// my way didnt work
+
+// function formatMoney(amount){
+  
+//   let newArray = ["$"]
+//   newArray.push(amount)
+//   if(newArray.slice('.'))
+//   return newArray.join('').toString()
+
+// }
