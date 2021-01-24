4dba8adf11f63998b2fa76120ec1cdbbc6d72fbd
Saji <sewangco@gmail.com>
2021-01-24 17:53:18 +0100

    complementaryDNA

---
+++ 7kyu/complementaryDNA.js
@@ -0,0 +1,26 @@
+function DNAStrand(dna){
+    let newDna= []
+    let array = dna.split('')
+    array.forEach(char => {
+      if (char === 'A'){
+        newDna.push('T') 
+      }else if(char === 'T'){
+        newDna.push('A') 
+      } else if(char === 'G'){
+        newDna.push('C') 
+      } else if(char === 'C'){
+        newDna.push('G') 
+      } 
+    })
+    return newDna.join('')
+  }
+  //DNAStrand("GTAT")
+  //,"CATA",
+
+  //alt 
+
+  var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
