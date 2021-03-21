0c8b0983a1a538b3daa96d5670e99f1c1fe40c49
Saji <sewangco@gmail.com>
2021-03-21 23:04:44 +0100

    removeSMallest

--- 7kyu/removeTheMinimum.js
+++ 7kyu/removeTheMinimum.js
@@ -3,3 +3,8 @@ function removeSmallest(numbers) {
   
   }
   removeSmallest([5, 3, 2, 1, 4])
+
+  function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+  }
