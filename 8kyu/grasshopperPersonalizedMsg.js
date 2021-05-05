070727789565af93aed2e4bd3ce44616aeecc7dd
Saji <sewangco@gmail.com>
2021-05-05 21:11:22 +0200

    grasshopperPersonalizedMsg2

--- 8kyu/grasshopperPersonalizedMsg.js
+++ 8kyu/grasshopperPersonalizedMsg.js
@@ -1,10 +1,14 @@
-function greet (name, owner) {
-    if(name===owner){
-      return "Hello boss"
-    }else{
-      return "Hello guest"
-    }
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss"
+  } else {
+    return "Hello guest"
   }
+}
 
-  greet('Greg', 'Daniel')
-  //'Hello guest'
+greet('Greg', 'Daniel')
+//'Hello guest'
+
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : 'Hello guest'
+}
