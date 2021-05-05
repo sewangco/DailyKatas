d32ff24bee67a16d6d4455b3f335c525c94e2015
Saji <sewangco@gmail.com>
2021-05-05 21:17:17 +0200

    oppositesAttract2

--- 8kyu/oppositesAttract.js
+++ 8kyu/oppositesAttract.js
@@ -2,3 +2,13 @@ function lovefunc(flower1, flower2){
     return flower1%2 !== flower2%2 ? true : false
   
   }
+
+  function lovefunc(flower1, flower2){
+    if(flower1%2===0 && flower2%2!==0 ||flower1%2!==0 && flower2%2===0 ){
+      return true
+    } 
+    return false
+  }
+
+  lovefunc(1,4)
+//true
