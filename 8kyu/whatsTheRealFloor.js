2e29497335cbfe5f363fcaeaa8b1e4697a3d07c2
Saji <sewangco@gmail.com>
2021-05-08 21:43:12 +0200

    whatsTheRealFloor

---
+++ 8kyu/whatsTheRealFloor.js
@@ -0,0 +1,15 @@
+// function getRealFloor(n) {
+//   if(n >13){
+//     return n-2
+//   } else if(n< 13 && n >0 ){
+//     return n-1
+//   } else{
+//     return n
+//   }
+// }
+
+function getRealFloor(n) {
+    return n >13? n-2 : n>0 ? n-1 : n
+  }
+  getRealFloor(-1)
+  //4
